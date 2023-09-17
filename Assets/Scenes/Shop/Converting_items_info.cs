ae9f95fd2428483136f26b1987e008ee5982ad25
Juesgape <juesgape11@gmail.com>
2023-09-17 10:45:13 -0500

    Equipping skins

--- Assets/Scenes/Shop/Converting_items_info.cs
+++ Assets/Scenes/Shop/Converting_items_info.cs
@@ -1,5 +1,3 @@
- using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,12 +8,23 @@ public class ConvertingItemsInfo : MonoBehaviour
     public TextMeshProUGUI priceText;
     public TextMeshProUGUI title;
     public Button buyButton;
+    public Button equipButton;
+
     int price;
     int totalMoney;
-    // Start is called before the first frame update
+
+    //What is the current skin?
+    private enum CurrentSkin
+    {
+        defaultSkin,
+        redSkin,
+        blueSkin
+    }
+
     void Start()
     {
         price = int.Parse(priceText.text);
+        equipButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,9 +37,25 @@ public class ConvertingItemsInfo : MonoBehaviour
         }
     }
 
+    //Onclick function that buys items whenever the user clicks the buy item button
     public void BuyItem()
     {
         totalMoney -= price;
         PlayerPrefs.SetInt("totalMoney", totalMoney);
+
+        //Hiding the buy button cuz we already buyed the item
+        buyButton.gameObject.SetActive(false);
+
+        //Making equipButton visible and adding it an eventListener
+        equipButton.gameObject.SetActive(true);
+        equipButton.onClick.AddListener(EquipSkin);
+    }
+
+    public void EquipSkin()
+    {
+        equipButton.GetComponentInChildren<TextMeshProUGUI>().text = "Equipped :)";
+        equipButton.interactable = false;
+
+        Debug.Log("Your new skin is ", title);
     }
 }
